4d2f6d36880ad66c8d76d4317235886cd134483d
Patrik <patrik95195@gmail.com>
2021-08-09 12:21:58 +0200

    fix shot angle and add xg text when tagging shot

--- app.py
+++ app.py
@@ -8,27 +8,26 @@ model = load('model.joblib')
 
 @app.route('/predict', methods=['POST'])
 def predict():
-    location = request.get_json()
-    x = to_features(location)
+    shot_location = request.get_json()
+    x = to_features(shot_location)
     x = np.array(x).reshape(1, -1)
-    xg = model.predict_proba(x)[:, 1][0]
+    xg = round(model.predict_proba(x)[:, 1][0], 2)
     print(xg)
     return {'xg': xg}
 
-def to_features(location):
-    destination = np.array([105, 34])
-    distance = np.sqrt(np.sum((location - destination) ** 2))
+def to_features(shot_location):
+    goal_location = np.array([105, 34])
+    distance = np.sqrt(np.sum((shot_location - goal_location) ** 2))
 
-    p0 = np.array((105, 34 - 7.32 / 2))
-    p1 = np.array(location, dtype=np.float)
-    p2 = np.array((105, 34 + 7.32 / 2))
+    goal_width = 7.32
+    dx = shot_location[0] - goal_location[0]
+    dy1 = shot_location[1] - (goal_location[1] + goal_width / 2)
+    dy2 = shot_location[1] - (goal_location[1] - goal_width / 2)
+    u = np.array([dx, dy1])
+    v = np.array([dx, dy2])
+    angle = np.arccos((u @ v) / (np.linalg.norm(u) * np.linalg.norm(v)))
 
-    v0 = p0 - p1
-    v1 = p2 - p1
-
-    angle = np.abs(np.math.atan2(np.linalg.det([v0, v1]), np.dot(v0, v1)))
-
-    return [*location, distance, angle]
+    return [*shot_location, distance, angle]
 
 
 @app.route('/')
