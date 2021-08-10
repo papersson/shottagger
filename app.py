5c517d8ca86d36ea81bf2967bc4270efbec6421d
Patrik <patrik95195@gmail.com>
2021-08-10 11:09:42 +0200

    first iteration done

--- app.py
+++ app.py
@@ -8,14 +8,16 @@ model = load('model.joblib')
 
 @app.route('/predict', methods=['POST'])
 def predict():
-    shot_location = request.get_json()
-    x = to_features(shot_location)
+    data = request.get_json()
+    print(data)
+    x = to_features(data)
     x = np.array(x).reshape(1, -1)
     xg = round(model.predict_proba(x)[:, 1][0], 2)
     print(xg)
     return {'xg': xg}
 
-def to_features(shot_location):
+def to_features(data):
+    shot_location = data['shotLocation']
     goal_location = np.array([105, 34])
     distance = np.sqrt(np.sum((shot_location - goal_location) ** 2))
 
@@ -27,7 +29,27 @@ def to_features(shot_location):
     v = np.array([dx, dy2])
     angle = np.arccos((u @ v) / (np.linalg.norm(u) * np.linalg.norm(v)))
 
-    return [*shot_location, distance, angle]
+    defenders = data['defenders']
+    n_defenders = defenders_in_fov(defenders, shot_location)
+    return [*shot_location, distance, angle, n_defenders]
+
+def point_in_triangle(p0, p1, p2, p):
+    # https://stackoverflow.com/questions/2049582/how-to-determine-if-a-point-is-in-a-2d-triangle
+    area = 0.5 *(-p1[1]*p2[0] + p0[1]*(-p1[0] + p2[0]) + p0[0]*(p1[1] - p2[1]) + p1[0]*p2[1]);
+    s = 1/(2*area)*(p0[1]*p2[0] - p0[0]*p2[1] + (p2[1]- p0[1])*p[0] + (p0[0] - p2[0])*p[1]);
+    t = 1/(2*area)*(p0[0]*p1[1] - p0[1]*p1[0] + (p0[1] - p1[1])*p[0] + (p1[0] - p0[0])*p[1]);
+    return s > 0 and t > 0 and 1 - s - t > 0
+
+def defenders_in_fov(defenders, shot_location):
+    goal_width = 7.32
+
+    p1 = np.array([105, 34 + goal_width / 2])
+    p2 = np.array([105, 34 - goal_width / 2])
+    count = 0
+    for defender in defenders:
+        if point_in_triangle(shot_location, p1, p2, defender):
+            count += 1
+    return count
 
 
 @app.route('/')
