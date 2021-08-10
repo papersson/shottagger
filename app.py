b53e2c9b5efec5a8d77f47c67d5e57cc23d85b8f
Patrik <patrik95195@gmail.com>
2021-08-10 13:03:41 +0200

    fix syntax error

--- app.py
+++ app.py
@@ -56,5 +56,5 @@ def home():
     return render_template('index.html')
 
 if __name__ == '__main__':
-    p = int(os.environ.get('PORT', 5000)
+    p = int(os.environ.get('PORT', 5000))
     app.run(debug=True, port=p, host='0.0.0.0')
